2a38fd027e1b1e84efb843cc5e24d048c546cff2
Matias Salazar <matias39974593@gmail.com>
2024-08-29 13:02:55 -0300

    se realizo un primer componente que muestra en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
-  return <>
+  return <section className='container my-5'>
+    <h1 className='text-center'>Hello World!</h1>
+  </section>
+
   
-  </>;
 }
 
 export default App;
